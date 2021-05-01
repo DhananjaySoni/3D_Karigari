6b027ede7eb2669174b0fc936159eff8fba4057f
yug chandak <yugchandak1@gmail.com>
2021-05-02 04:34:30 +0530

    Contact us form

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,20 +1,114 @@
 <template>
-    <div class='navbar'>
-            <nav>
-                <div class='navbar_header'>
-                    <p>3D</p>
-                </div>
-                <ul class='nav-links'>
-                    <li class="navlink"><router-link to="/">Home</router-link></li>
-                    <li class="navlink"><router-link to="/products">Products</router-link></li>
-                    <li class="navlink"><router-link to="/">Contact</router-link></li>
-                </ul>
-            </nav>
-        </div>
+    <div class="navbar">
+        <nav>
+            <div class="navbar_header">
+                <p>3D</p>
+            </div>
+            <ul class="nav-links">
+                <li class="navlink"><router-link to="/">Home</router-link></li>
+                <li class="navlink"><router-link to="/products">Products</router-link></li>
+                <li class="navlink"><router-link to="/" v-b-modal.modal-scrollable1>Contact</router-link></li>
+            </ul>
+            <b-modal id="modal-scrollable1" scrollable size="xl" hide-footer>
+                <section class="ftco-section">
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-md-6 text-center mb-5">
+                                <h2 class="heading-section">Contact Us</h2>
+                            </div>
+                        </div>
+                        <div class="row justify-content-center">
+                            <div class="col-lg-10 col-md-12">
+                                <div class="wrapper">
+                                    <div class="row no-gutters">
+                                        <div class="col-md-7 d-flex align-items-stretch">
+                                            <div class="contact-wrap w-100 p-md-5 p-4">
+                                                <h3 class="mb-4">Get in touch</h3>
+                                                <div id="form-message-warning" class="mb-4"></div>
+                                                <div id="form-message-success" class="mb-4">
+                                                    Your message was sent, thank you!
+                                                </div>
+                                                <form method="POST" id="contactForm" name="contactForm" novalidate="novalidate">
+                                                    <div class="row">
+                                                        <div class="col-md-6">
+                                                            <div class="form-group">
+                                                                <input type="text" class="form-control" name="name" id="name" placeholder="Name" />
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-md-6">
+                                                            <div class="form-group">
+                                                                <input type="email" class="form-control" name="email" id="email" placeholder="Email" />
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-md-12">
+                                                            <div class="form-group">
+                                                                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" />
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-md-12">
+                                                            <div class="form-group">
+                                                                <textarea name="message" class="form-control" id="message" cols="30" rows="7" placeholder="Message"></textarea>
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-md-12">
+                                                            <div class="form-group">
+                                                                <input type="submit" value="Send Message" class="btn btn-primary" />
+                                                                <div class="submitting"></div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-5 d-flex align-items-stretch">
+                                            <div class="info-wrap bg-primary w-100 p-lg-5 p-4">
+                                                <h3 class="mb-4 mt-md-4">Contact us</h3>
+                                                <div class="dbox w-100 d-flex align-items-start">
+                                                    <div class="icon d-flex align-items-center justify-content-center">
+                                                        <span class="fa fa-map-marker"></span>
+                                                    </div>
+                                                    <div class="text pl-3">
+                                                        <p><span>Address:</span> 198 West 21th Street, Suite 721 New York NY 10016</p>
+                                                    </div>
+                                                </div>
+                                                <div class="dbox w-100 d-flex align-items-center">
+                                                    <div class="icon d-flex align-items-center justify-content-center">
+                                                        <span class="fa fa-phone"></span>
+                                                    </div>
+                                                    <div class="text pl-3">
+                                                        <p><span>Phone:</span> <a href="tel://1234567920">+ 1235 2355 98</a></p>
+                                                    </div>
+                                                </div>
+                                                <div class="dbox w-100 d-flex align-items-center">
+                                                    <div class="icon d-flex align-items-center justify-content-center">
+                                                        <span class="fa fa-paper-plane"></span>
+                                                    </div>
+                                                    <div class="text pl-3">
+                                                        <p><span>Email:</span> <a href="mailto:info@yoursite.com">info@yoursite.com</a></p>
+                                                    </div>
+                                                </div>
+                                                <div class="dbox w-100 d-flex align-items-center">
+                                                    <div class="icon d-flex align-items-center justify-content-center">
+                                                        <span class="fa fa-globe"></span>
+                                                    </div>
+                                                    <div class="text pl-3">
+                                                        <p><span>Website</span> <a href="#">yoursite.com</a></p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </b-modal>
+        </nav>
+    </div>
 </template>
 
 <style scoped>
-
 .navbar {
     background: #121212;
     position: absolute;
@@ -65,7 +159,7 @@ ul.nav-links li {
     margin: auto 3vw;
 }
 
-ul li a{
+ul li a {
     text-decoration: none;
     color: white;
     /* margin-right:5vw; */
@@ -82,13 +176,13 @@ ul li button {
     color: white;
 }
 
-@media screen and (max-width: 576px){
+@media screen and (max-width: 576px) {
     nav {
         font-size: 80%;
     }
 }
 
-@media screen and (max-width: 400px){
+@media screen and (max-width: 400px) {
     nav {
         display: block;
         align-items: center;
@@ -102,7 +196,7 @@ ul li button {
         margin-left: 0;
     }
 
-    ul.nav-links{
+    ul.nav-links {
         width: 100%;
         margin: auto;
     }
